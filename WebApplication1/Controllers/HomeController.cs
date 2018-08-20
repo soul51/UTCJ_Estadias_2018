67ee30514af4445209fb85c6e1c29d815e737441
Manuel Gutierrez <warmachine2005@hotmail.com>
2018-08-20 11:48:48 -0600

    Se agregaron % en cursos

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -100,7 +100,7 @@ namespace WebApplication1.Controllers {
         }
         public ActionResult Descuentos()
         {
-            ViewBag.Message = "";
+            ViewBag.Message = " ";
 
             return View();
         }
